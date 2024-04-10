4bf8c014517a0c4d7609f299cb9badc9e82b77c2
Andy123qq4 <1394568964@qq.com>
2024-04-10 21:19:23 +0800

    Fix Ui and UiTest for gradle checking

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -1,6 +1,6 @@
 package seedu.duke;
 
-import java.time.format.DateTimeFormatter;
+//import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,16 +16,21 @@ public class Ui {
     private static final String INPUT_INSTRUCTION =
             "Input Instructions:\n" +
                     "1. [operators]: Choose from +, -, *, /. You can use any combination of them.\n" +
-                    "2. [number]: Specify the number of problems to generate. Must be between 1 and 100.\n" +
-                    "3. [maximum digit]: Set the maximum number of digits for the numbers in the problems. Must be between 1 and 9.\n" +
-                    "4. [number of operands]: Determine the number of operands in each problem. Must be between 2 and 10.\n\n" +
+                    "2. [number]: Specify the number of problems to generate. " +
+                    "Must be between 1 and 100.\n" +
+                    "3. [maximum digit]: Set the maximum number of digits for the numbers in the problems. " +
+                    "Must be between 1 and 9.\n" +
+                    "4. [number of operands]: Determine the number of operands in each problem. " +
+                    "Must be between 2 and 10.\n\n" +
                     "Example Command: generate -t + -n 10 -d 2 -l 2\n" +
-                    "This will generate 10 problems using the + operator only. Each problem will have 2 operands, and the maximum number of digits for each operand is 2 (i.e., up to 99).";
+                    "This will generate 10 problems using the + operator only. " +
+                    "Each problem will have 2 operands, and the maximum number of digits for each operand is 2.";
     private static final String GEN_COMMAND =
             "Generate problem sets: \t" + "gen -t [type] -n [number] -d [maximum digits]";
     private static final String HELP_COMMAND =
             "Help Function: \n" +
-                    "To get help on a specific command, type 'help' followed by the command name. Here are the available commands:\n" +
+                    "To get help on a specific command, type 'help' followed by the command name. " +
+                    "Here are the available commands:\n" +
                     "1. 'generate' or 'gen': Generate a problem set based on a series of parameters.\n" +
                     "2. 'records': View the records of your past problem solving sessions.\n" +
                     "3. 'retry': Retry a problem set you have solved before.\n" +
@@ -42,8 +47,6 @@ public class Ui {
             "Retry a problem set you have solved before in the past: \t" + "retry PROBLEM_SET_ID\n" +
                     "PROBLEM_SET_ID is an integer, and can be found by using the 'records' command.\n";
     private static final String EXIT_COMMAND = "Exit program: exit\n";
-    // msg for other classes
-//    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final String name;
     private final Scanner scanner = new Scanner(System.in);
