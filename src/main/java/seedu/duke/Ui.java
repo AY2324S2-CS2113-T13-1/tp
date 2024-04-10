f092e09721be648648d68a2264e2756465066259
Andy123qq4 <1394568964@qq.com>
2024-04-10 17:38:30 +0800

    Better readability in UI and better help function

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -3,6 +3,7 @@ package seedu.duke;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 /**
  * Represents the user interface for interacting with the chatbot.
  */
@@ -10,33 +11,39 @@ public class Ui {
 
     // Pre-defined sentences
     private static final String PROBLEM_FORM =
-             "To generate a problem set: Input the number and difficulty like below: \n" +
-                     "< generate -t [type] -n [number] -d [maximum digit] -l [number of operands] >";
+            "To generate a problem set: Input the number and difficulty like below: \n" +
+                    "< generate -t [type] -n [number] -d [maximum digit] -l [number of operands] >";
     private static final String INPUT_INSTRUCTION =
             "Input Instructions:\n" +
-                    "[operators]: can be + - * /, you can combine any of them.\n" +
-                    "[number]: number of problem set generated\n" +
-                    "[maximum digit]: how big can the calculation be\n\n" +
-                    "For example: generate -t + -n 10 -d 2 -l 2\n" +
-                    "-> generate 10 problems with + and - operator, each has 2 numbers taking operations\n"+
-                    "and the maximum number of digits is 2 (99 max)";
+                    "1. [operators]: Choose from +, -, *, /. You can use any combination of them.\n" +
+                    "2. [number]: Specify the number of problems to generate. Must be between 1 and 100.\n" +
+                    "3. [maximum digit]: Set the maximum number of digits for the numbers in the problems. Must be between 1 and 9.\n" +
+                    "4. [number of operands]: Determine the number of operands in each problem. Must be between 2 and 10.\n\n" +
+                    "Example Command: generate -t + -n 10 -d 2 -l 2\n" +
+                    "This will generate 10 problems using the + operator only. Each problem will have 2 operands, and the maximum number of digits for each operand is 2 (i.e., up to 99).";
     private static final String GEN_COMMAND =
             "Generate problem sets: \t" + "gen -t [type] -n [number] -d [maximum digits]";
     private static final String HELP_COMMAND =
-            "Help function: \t" + "help [type], type can be 'gen'/'records'/...";
+            "Help Function: \n" +
+                    "To get help on a specific command, type 'help' followed by the command name. Here are the available commands:\n" +
+                    "1. 'generate' or 'gen': Generate a problem set based on a series of parameters.\n" +
+                    "2. 'records': View the records of your past problem solving sessions.\n" +
+                    "3. 'retry': Retry a problem set you have solved before.\n" +
+                    "4. 'exit': Exit the program.\n" +
+                    "For example, 'help generate' will show you how to use the generate command.";
     private static final String RECORDS_COMMAND =
             "View past records(in-brackets parameters are optional): \t" + "records [-d] [-s] [-a] [-p] [-details]\n" +
-                    "[-d]: sort the records based on dateTime in increasing order. Add r to reverse order (-dr)\n" +
-                    "[-s]: sort the records based on speed in increasing order. Add r to reverse order (-sr)\n" +
-                    "[-a]: sort the records based on accuracy in increasing order. Add r to reverse order (-ar)\n" +
-                    "[-p]: sort the records based on problemSetID in increasing order. Add r to reverse order (-pr)\n" +
-                    "[-details]: show the details of the problem set(each individual problem).";
+                    "[-d]: Sort the records based on dateTime in increasing order. Add r to reverse order (-dr)\n" +
+                    "[-s]: Sort the records based on speed in increasing order. Add r to reverse order (-sr)\n" +
+                    "[-a]: Sort the records based on accuracy in increasing order. Add r to reverse order (-ar)\n" +
+                    "[-p]: Sort the records based on problemSetID in increasing order. Add r to reverse order (-pr)\n" +
+                    "[-details]: Show the details of the problem set(each individual problem).";
     private static final String RETRY_COMMAND =
             "Retry a problem set you have solved before in the past: \t" + "retry PROBLEM_SET_ID\n" +
                     "PROBLEM_SET_ID is an integer, and can be found by using the 'records' command.\n";
     private static final String EXIT_COMMAND = "Exit program: exit\n";
     // msg for other classes
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final String name;
     private final Scanner scanner = new Scanner(System.in);
@@ -49,16 +56,6 @@ public class Ui {
     public Ui(String name) {
         this.name = name;
     }
-
-    static void missingMessage(String parameters){
-
-        System.out.println(" Using default " + parameters);
-    }
-    static void invalidMessage(String parameters){
-
-        System.out.println(" invalid " + parameters + "!");
-    }
-
 
     /**
      * Displays a greeting message.
@@ -73,9 +70,8 @@ public class Ui {
                 "|_|  |_|\\__,_|\\__|_| |_|\\____|\\___|_| |_|_|\\__,_|___/\n";
         System.out.println(logo);
         System.out.println("Hello! I'm " + name + "!");
-        System.out.println("Type 'help' to see the instructions. \n");
+        System.out.println("Type 'help' to see all instructions. \n");
         showLine();
-        // System.out.println(PROBLEM_FORM);
     }
 
     public String readCommand() {
@@ -140,6 +136,14 @@ public class Ui {
         System.out.println("Invalid parameters for command: " + command);
         System.out.println("Try 'help COMMAND_NAME' for proper command formats");
         showLine();
+    }
+
+    static void invalidMessage(String parameters) {
+        System.out.println("Detected invalid " + parameters + "!");
+    }
+
+    static void missingMessage(String parameters) {
+        System.out.println("Using default " + parameters + " instead!");
     }
 
     public void exit() {
