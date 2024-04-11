e94b0ee9d2f2217b2f827d83006b0936fbe50ca3
Andy123qq4 <1394568964@qq.com>
2024-04-11 22:20:15 +0800

    Add first time generate problem instruction

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 public class Ui {
 
     // Pre-defined sentences
-    private static final String PROBLEM_FORM =
-            "To generate a problem set: Input the number and difficulty like below: \n" +
+    private static final String FIRSTTIME_INSTRUCTION =
+            "First time instruction: To generate a problem set, Input like below: \n" +
                     "< generate -t [type] -n [number] -d [maximum digit] -l [number of operands] >";
     private static final String INPUT_INSTRUCTION =
             "Input Instructions:\n" +
@@ -47,7 +47,6 @@ public class Ui {
                     "PROBLEM_SET_ID is an integer, and can be found by using the 'records' command.\n";
     private static final String EXIT_COMMAND = "Exit program: exit\n";
 
-
     private final String name;
     private final Scanner scanner = new Scanner(System.in);
 
@@ -74,6 +73,7 @@ public class Ui {
         System.out.println(logo);
         System.out.println("Hello! I'm " + name + "!");
         System.out.println("Type 'help' to see all instructions. \n");
+        System.out.println(FIRSTTIME_INSTRUCTION);
         showLine();
     }
 
